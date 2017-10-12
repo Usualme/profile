95864a5750194527c4d7fba08ebff42fdae7ceaa
Usual <532000663@qq.com>
2017-10-12 17:10:14 +0800

    add mongodb and update mysql for docker service

--- docker/mysql.sh
+++ docker/mysql.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # docker mysql
-docker run -p 3306:3306 -v /var/lib/mysql:/var/lib/mysql -v /etc/mysql/my.cnf:/etc/mysql/my.cnf --env MYSQL_ROOT_PASSWORD=adminn mariadb:10.1.21
+docker run -p 3306:3306 -v ~/mysql:/var/lib/mysql --name mysql -v ~/mysql/my.cnf:/etc/mysql/my.cnf --env MYSQL_ROOT_PASSWORD=adminn mariadb
