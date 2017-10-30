fc246a10761f1b33eded62b164d9dc1a6de57527
Usual <532000663@qq.com>
2017-10-31 01:31:34 +0800

    update jdk install

--- linux/jdk.sh
+++ linux/jdk.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 wget --header "Cookie: oraclelicense=accept-securebackup-cookie" \
-    http://download.oracle.com/otn-pub/java/jdk/8u5-b13/jdk-8u5-linux-x64.tar.gz
+http://download.oracle.com/otn-pub/java/jdk/8u151-b12/e758a0de34e24606bca991d704f6dcbf/jdk-8u151-linux-x64.tar.gz
 
-sudo tar zxvf jdk-8u5-linux-x64.tar.gz -C /opt
+sudo tar zxvf jdk-8u151-linux-x64.tar.gz -C /opt
 
-update-alternatives --install /usr/bin/java java /opt/jdk1.8.0_05/bin/java 100
-update-alternatives --install /usr/bin/javac javac /opt/jdk1.8.0_05/bin/javac 100
+sudo update-alternatives --install /usr/bin/java java /opt/jdk1.8.0_151/bin/java 100
+sudo update-alternatives --install /usr/bin/javac javac /opt/jdk1.8.0_151/bin/javac 100
 
-echo 'export JAVA_HOME=/opt/jdk1.8.0_05' >> ~/.zshrc
+echo 'export JAVA_HOME=/opt/jdk1.8.0_151' >> ~/.zshrc
